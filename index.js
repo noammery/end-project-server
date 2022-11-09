c2114bf8ea1ec0581f34c1b06838b56fc07b534d
noammery <noammery55@gmail.com>
2022-11-09 15:24:45 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require(`express`);
+const port = 3001;
+const { default: mongoose } = require("mongoose");
+const app = express();
+const cors = require(`cors`);
+const bodyParser = require(`body-parser`);
+require(`dotenv`).config();
+
+mongoose.Promise = global.Promise;
+mongoose
+  .connect(
+    "mongodb+srv://admin:admin@cluster0.e32pweb.mongodb.net/?retryWrites=true&w=majority",
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log(`Connected to Data-base`))
+  .catch((err) => console.log(err));
+
+app.use(cors());
+
+app.use(bodyParser.json());
+
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
