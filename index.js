6c1ef565bb768c15b26562d2841febd316e5707d
ChimaCuma <oleg.bragin.01@gmail.com>
2022-11-10 11:11:47 +0200

    first-commit

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
-const express = require(`express`);
-const port = 3001;
-const { default: mongoose } = require("mongoose");
-const app = express();
-const cors = require(`cors`);
-const bodyParser = require(`body-parser`);
-require(`dotenv`).config();
+const express = require('express')
+const mongoose = require('mongoose')
+const authRouter = require('./routes/authRouter')
+const eventRouter = require('./routes/eventRouter')
+const { DB } = require("./config")
+const cors = require('cors');
 
-mongoose.Promise = global.Promise;
-mongoose
-  .connect(
-    "mongodb+srv://admin:admin@cluster0.e32pweb.mongodb.net/?retryWrites=true&w=majority",
-    { useNewUrlParser: true }
-  )
-  .then(() => console.log(`Connected to Data-base`))
-  .catch((err) => console.log(err));
+
+const PORT = 5000
+
+const app = express()
 
 app.use(cors());
 
-app.use(bodyParser.json());
+app.use(express.json())
 
-app.listen(port, () => {
-  console.log(`server is running on port ${port}`);
-});
+app.use("/auth", authRouter)
+
+app.use("/add", eventRouter)
+
+const start = async () => {
+    try {
+        await mongoose.connect(DB)
+        app.listen(PORT, () => console.log(`server started on port ${PORT}`))
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+start()
