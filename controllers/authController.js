a00540b1ccea3d363e8020ec7375948030b3e58e
noammery <noammery55@gmail.com>
2022-11-14 09:29:30 +0200

    socket start

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,87 +1,127 @@
-const User = require('../models/User')
-const Role = require('../models/Role')
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
-const { validationResult } = require('express-validator')
-const { secret } = require("../config")
+const User = require("../models/User");
+const Role = require("../models/Role");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { validationResult } = require("express-validator");
+const { secret } = require("../config");
 
 const generateAccessToken = (id, role) => {
-    const payload = {
-        id,
-        role
-    }
-    return jwt.sign(payload, secret, { expiresIn: "24h" })
-}
+  const payload = {
+    id,
+    role,
+  };
+  return jwt.sign(payload, secret, { expiresIn: "24h" });
+};
 
 class authController {
+  async registration(req, res) {
+    try {
+      const errors = validationResult(req);
 
-    async registration(req, res) {
-        try {
-            const errors = validationResult(req)
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ message: "Registration error", errors });
+      }
 
-            if (!errors.isEmpty()) {
-                return res.status(400).json({ message: "Registration error", errors })
-            }
+      const {
+        username,
+        password,
+        role,
+        image,
+        fullname,
+        email,
+        phone,
+        birthday,
+        department,
+        status,
+        sex,
+      } = req.body;
+      const candidate = await User.findOne({ username });
 
-            const { username, password, role, image, fullname, email, phone, birthday, department, status, sex } = req.body
-            const candidate = await User.findOne({ username })
+      if (candidate) {
+        return res.status(400).json({ message: "User already exists" });
+      }
 
-            if (candidate) {
-                return res.status(400).json({ message: "User already exists" })
-            }
+      const hashPassword = bcrypt.hashSync(password, 7);
+      const user = new User({
+        username,
+        password: hashPassword,
+        role,
+        image,
+        fullname,
+        email,
+        phone,
+        birthday,
+        department,
+        status,
+        sex,
+      });
+      await user.save();
 
-            const hashPassword = bcrypt.hashSync(password, 7)
-            const user = new User({ username, password: hashPassword, role, image, fullname, email, phone, birthday, department, status, sex  })
-            await user.save()
-            
-            return res.json({ message: "User registered successfully" })
-
-        } catch (e) {
-            console.log(e);
-            res.status(400).json({ message: "Registration error" })
-        }
+      return res.json({ message: "User registered successfully" });
+    } catch (e) {
+      console.log(e);
+      res.status(400).json({ message: "Registration error" });
     }
+  }
 
-    async login(req, res) {
-        try {
-            const { mail, password } = req.body
-            const user = await User.findOne({ mail })
+  async login(req, res) {
+    try {
+      const { mail, password } = req.body;
+      const user = await User.findOne({ mail });
 
-            if (!user) {
-                return res.status(400).json({ message: `User ${mail} not found` })
-            }
+      if (!user) {
+        return res.status(400).json({ message: `User ${mail} not found` });
+      }
 
-            const validPassword = bcrypt.compareSync(password, user.password)
+      const validPassword = bcrypt.compareSync(password, user.password);
 
-            if (!validPassword) {
-                return res.status(400).json({ message: `Invalid password` })
-            }
-            const token = generateAccessToken(user._id, user.role)
-            const {role, image, fullname, email, phone, birthday, department, status, sex} = user
-            return res.json({ token, role, image, fullname, email, phone, birthday, department, status, sex })
-
-        } catch (e) {
-            console.log(e);
-            res.status(400).json({ message: "Login error" })
-        }
+      if (!validPassword) {
+        return res.status(400).json({ message: `Invalid password` });
+      }
+      const token = generateAccessToken(user._id, user.role);
+      const {
+        role,
+        image,
+        fullname,
+        email,
+        phone,
+        birthday,
+        department,
+        status,
+        sex,
+      } = user;
+      return res.json({
+        token,
+        role,
+        image,
+        fullname,
+        email,
+        phone,
+        birthday,
+        department,
+        status,
+        sex,
+      });
+    } catch (e) {
+      console.log(e);
+      res.status(400).json({ message: "Login error" });
     }
-    async getUsers(req, res) {
-        const sent = {}
-        try {
-            const users = await User.find()
-
-            res.json(users)
-        } catch (e) {
-            console.log(e);
-        }
+  }
+  async getUsers(req, res) {
+    const sent = {};
+    try {
+      const users = await User.find();
+      res.json(users);
+    } catch (e) {
+      console.log(e);
     }
+  }
 
-    async makeAdmin(req, res){
-        try {
-            
-        } catch (e) {
-            console.log(e);
-        }
+  async makeAdmin(req, res) {
+    try {
+    } catch (e) {
+      console.log(e);
     }
+  }
 }
-module.exports = new authController()       
+module.exports = new authController();
--- index.js
+++ index.js
@@ -1,33 +1,61 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const authRouter = require('./routes/authRouter')
-const eventRouter = require('./routes/eventRouter')
-const departAdminRouter = require('./routes/departAdminRouter')
-const { DB } = require("./config")
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const authRouter = require("./routes/authRouter");
+const eventRouter = require("./routes/eventRouter");
+const departAdminRouter = require("./routes/departAdminRouter");
+const app = express();
+const http = require("http");
+const { Server } = require("socket.io");
+const { DB } = require("./config");
+const cors = require("cors");
 
-
-const PORT = 5000
-
-const app = express()
+const PORT = 5000;
 
 app.use(cors());
 
-app.use(express.json())
+app.use(express.json());
 
-app.use("/auth", authRouter)
+const server = http.createServer(app);
 
-app.use("/add", eventRouter)
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
 
-app.use("/departments", departAdminRouter)
+io.on("connection", (socket) => {
+  console.log(`user ${socket.id} connected`);
+
+  socket.on("join_room", () => {
+    socket.join("1");
+    console.log(`user${socket.id} joinned room 1`);
+  });
+
+  socket.on("send", (message) => {
+    console.log(message);
+    socket.to("1").emit("back", message);
+  });
+
+  socket.on(`disconnect`, () => {
+    console.log(`User  ${socket.id} disconnect`);
+  });
+});
+
+app.use("/auth", authRouter);
+
+app.use("/add", eventRouter);
+
+app.use("/departments", departAdminRouter);
 
 const start = async () => {
-    try {
-        await mongoose.connect(DB)
-        app.listen(PORT, () => console.log(`server started on port ${PORT}`))
-    } catch (e) {
-        console.log(e)
-    }
-}
+  try {
+    await mongoose.connect(DB);
+    server.listen(PORT, () => console.log(`server started on port ${PORT}`));
+  } catch (e) {
+    console.log(e);
+  }
+};
 
-start()
+start();
