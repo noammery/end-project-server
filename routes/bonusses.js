6ebcf2f296635203768cfabf46fe753e1ecc78da
noammery <noammery55@gmail.com>
2022-11-14 16:09:28 +0200

    delete bonuses

--- routes/bonusses.js
+++ routes/bonusses.js
@@ -1,41 +1,29 @@
-
-const Benifits = require('../models/bonusses.js')
-const express = require('express')
-
+const Benifits = require("../models/bonusses.js");
+const express = require("express");
 
 const benifitsRouter = express.Router();
 
-benifitsRouter.post(
-  '/get', (async (req, res) => {
-    const benifits = await Benifits.find();
-    res.send(benifits);
-  })
-);
+benifitsRouter.post("/get", async (req, res) => {
+  const benifits = await Benifits.find();
+  res.send(benifits);
+});
 
+benifitsRouter.post("/update", (req, res, next) => {
+  req.body.title &&
+  req.body.description &&
+  req.body.image &&
+  req.body.link &&
+  req.body.linktitle
+    ? Benifits.create(req.body)
+        .then((data) => res.json(data))
+        .catch(next)
+    : res.json({ error: "you have an error" });
+});
 
+benifitsRouter.delete("/delete/:title", (req, res, next) => {
+  Benifits.findOneAndDelete({ title: req.params.title })
+    .then((data) => res.json(data))
+    .catch(next);
+});
 
-benifitsRouter.post('/update', (req, res, next) => {
-  req.body.title && req.body.description && req.body.image && req.body.link && req.body.linktitle ?
-    Benifits.create(req.body)
-      .then((data) => res.json(data))
-      .catch(next) :
-    res.json({ error: 'you have an error' })
-})
-
-
-
-benifitsRouter.delete(
-  '/delete', (async (req, res) => {
-    const benifits = await Benifits.findOneAndDelete({ title: req.body.title });
-    if (benifits) {
-      res.send({ message: 'benifit Deleted' });
-    } else {
-      res.status(404).send({ message: 'benifit Not Found' });
-    }
-
-  })
-);
-
-module.exports = benifitsRouter
-
-
+module.exports = benifitsRouter;
