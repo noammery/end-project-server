040dad5178124fb55e08d4b4bdb97f9a23e081ba
noammery <noammery55@gmail.com>
2022-11-14 09:43:38 +0200

    error fix

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,9 @@
-
-const User = require('../models/User')
-const Role = require('../models/Role')
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
+const User = require("../models/User");
+const Role = require("../models/Role");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 // const { validationResult } = require('express-validator')
-const { secret } = require("../config")
-
+const { secret } = require("../config");
 
 const generateAccessToken = (id, role) => {
   const payload = {
@@ -18,31 +16,46 @@ const generateAccessToken = (id, role) => {
 class authController {
   async registration(req, res) {
     try {
-      const errors = validationResult(req);
+      // const errors = validationResult(req)
 
-    async registration(req, res) {
-        try {
-            // const errors = validationResult(req)
-
-            // if (!errors.isEmpty()) {
-            //     return res.status(400).json({ message: "Validation error", errors })
-            // }
-            console.log(req.body);
-            const {email, password, role, fullname, phone, birthday, department, sex, image, contract, adress} = req.body
-            const candidate = await User.findOne({ email })
-
+      // if (!errors.isEmpty()) {
+      //     return res.status(400).json({ message: "Validation error", errors })
+      // }
+      console.log(req.body);
+      const {
+        email,
+        password,
+        role,
+        fullname,
+        phone,
+        birthday,
+        department,
+        sex,
+        image,
+        contract,
+        adress,
+      } = req.body;
+      const candidate = await User.findOne({ email });
 
       if (candidate) {
         return res.status(400).json({ message: "User already exists" });
       }
 
-
-            const hashPassword = bcrypt.hashSync(password, 7)
-            const user = new User({ email, password:hashPassword, role, fullname, phone, birthday: new Date(), department, sex, image, contract, adress})
-            await user.save()
-            
-            return res.json({ message: "User registered successfully" })
-
+      const hashPassword = bcrypt.hashSync(password, 7);
+      const user = new User({
+        email,
+        password: hashPassword,
+        role,
+        fullname,
+        phone,
+        birthday: new Date(),
+        department,
+        sex,
+        image,
+        contract,
+        adress,
+      });
+      await user.save();
 
       return res.json({ message: "User registered successfully" });
     } catch (e) {
@@ -51,36 +64,53 @@ class authController {
     }
   }
 
+  async login(req, res) {
+    try {
+      const { email, password } = req.body;
+      const user = await User.findOne({ email });
 
-    async login(req, res) {
-        try {
-            const { email, password } = req.body
-            const user = await User.findOne({ email })
-
-            if (!user) {
-                return res.status(400).json({ message: `User ${email} not found` })
-            }
+      if (!user) {
+        return res.status(400).json({ message: `User ${email} not found` });
+      }
 
       const validPassword = bcrypt.compareSync(password, user.password);
 
-            if (!validPassword) {
-                return res.status(400).json({ message: `Invalid password` })
-            }
-            const token = generateAccessToken(user._id, user.role)
-            const {role, image, fullname, phone, birthday, department, status, sex} = user
-            return res.json({ token, role, image, fullname, email, phone, birthday, department, status, sex })
-
-        } catch (e) {
-            console.log(e);
-            res.status(400).json({ message: "Login error" })
-        }
-
+      if (!validPassword) {
+        return res.status(400).json({ message: `Invalid password` });
+      }
+      const token = generateAccessToken(user._id, user.role);
+      const {
+        role,
+        image,
+        fullname,
+        phone,
+        birthday,
+        department,
+        status,
+        sex,
+      } = user;
+      return res.json({
+        token,
+        role,
+        image,
+        fullname,
+        email,
+        phone,
+        birthday,
+        department,
+        status,
+        sex,
+      });
+    } catch (e) {
+      console.log(e);
+      res.status(400).json({ message: "Login error" });
     }
   }
   async getUsers(req, res) {
     const sent = {};
     try {
       const users = await User.find();
+
       res.json(users);
     } catch (e) {
       console.log(e);
