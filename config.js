f61e1ff73803273ecce004f08fe78ce7259bb9eb
ChimaCuma <oleg.bragin.01@gmail.com>
2022-11-16 16:01:40 +0200

    mail sent and distinct tested

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
     secret: "SECRET_KEY_RANDOM",
-    DB:"mongodb+srv://admin:admin@cluster0.jp8us9p.mongodb.net/?retryWrites=true&w=majority"
+    DB:"mongodb+srv://admin:admin@cluster0.jp8us9p.mongodb.net/?retryWrites=true&w=majority",
+    SENDGRID_API_KEY:'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'
 }
---
+++ mailSend.js
@@ -0,0 +1,17 @@
+const { SENDGRID_API_KEY } = require("./config");
+const sgMail = require('@sendgrid/mail')
+
+sgMail.setApiKey(SENDGRID_API_KEY)
+const msg = {
+  to: ['oleg.bragin.01@gmail.com','baxi585@gmail.com'], // Change to your recipient
+  from: 'dimonaWorkersSite@gmail.com', // Change to your verified sender
+  templateId: 'd-9d56dbe7304040858371bb8495de7705',
+}
+sgMail
+  .send(msg)
+  .then(() => {
+    console.log('Email sent')
+  })
+  .catch((error) => {
+    console.error(error)
+  })
