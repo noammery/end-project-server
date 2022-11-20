57ae82a96d6032b610f875faf628ad7dd4f7fd92
olegbergin <oleg.bragin.01@gmail.com>
2022-11-20 14:32:51 +0200

    port

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ require('dotenv').config()
 
 const cors = require("cors");
 const benifitsRouter = require("./routes/bonusses");
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const User = require("./models/User");
 
 
