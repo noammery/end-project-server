e8e7b82c87533fd443a92ca2ff4c06f4649cfbdb
noammery <noammery55@gmail.com>
2022-11-22 11:13:39 +0200

    validation

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,21 +1,20 @@
-const jwt = require('jsonwebtoken')
-require('dotenv').config()
-
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 module.exports = function (req, res, next) {
-    if (req.method === "OPTIONS") {
-        next()
+  if (req.method === "OPTIONS") {
+    next();
+  }
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    if (!token) {
+      return res.status(403).json({ message: "משתמש לא מורשה" });
     }
-    try {
-        const token = req.headers.authorization.split(' ')[1]
-        if (!token) {
-            return res.status(403).json({ message: "User is not authorized" })
-        }
-        const decodedData = jwt.verify(token, process.env.SECRET)
-        req.user = decodedData
-        next()
-    } catch (e) {
-        console.log(e);
-        return res.status(403).json({ message: "User is not authorized" })
-    }
-}
+    const decodedData = jwt.verify(token, process.env.SECRET);
+    req.user = decodedData;
+    next();
+  } catch (e) {
+    console.log(e);
+    return res.status(403).json({ message: "משתמש לא מורשה" });
+  }
+};
--- middlewares/roleMiddleware.js
+++ middlewares/roleMiddleware.js
@@ -1,30 +1,30 @@
-const jwt = require('jsonwebtoken')
-require('dotenv').config()
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 module.exports = function (roles) {
-    return function (req, res, next) {
-        if (req.method === "OPTIONS") {
-            next()
-        }
-        try {
-            const token = req.headers.authorization.split(' ')[1]
-            if (!token) {
-                return res.status(403).json({ message: "User is not authorized" })
-            }
-            const { roles: userRoles } = jwt.verify(token, process.env.SECRET)
-            let hasRole = false
-            userRoles.forEach(role => {
-                if (roles.includes(role)) {
-                    hasRole = true
-                }
-            })
-            if (!hasRole) {
-                return res.status(403).json({ message: "Access is not permitted" })
-            }
-            next()
-        } catch (e) {
-            console.log(e);
-            return res.status(403).json({ message: "User is not authorized" })
-        }
+  return function (req, res, next) {
+    if (req.method === "OPTIONS") {
+      next();
     }
-}
+    try {
+      const token = req.headers.authorization.split(" ")[1];
+      if (!token) {
+        return res.status(403).json({ message: "משתמש לא מורשה" });
+      }
+      const { roles: userRoles } = jwt.verify(token, process.env.SECRET);
+      let hasRole = false;
+      userRoles.forEach((role) => {
+        if (roles.includes(role)) {
+          hasRole = true;
+        }
+      });
+      if (!hasRole) {
+        return res.status(403).json({ message: "רשות לא ניתנת" });
+      }
+      next();
+    } catch (e) {
+      console.log(e);
+      return res.status(403).json({ message: "משתמש לא מורשה" });
+    }
+  };
+};
