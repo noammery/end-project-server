839a51b77671e4a6897943e0fe01a05343cf3f73
olegbergin <oleg.bragin.01@gmail.com>
2022-11-23 14:06:39 +0200

    everything wotking fix

--- controllers/departPostController.js
+++ controllers/departPostController.js
@@ -1,6 +1,7 @@
 const DepartmentPost = require("../models/DepartmentPosts");
 const { validationResult } = require("express-validator");
 require("dotenv").config();
+const User = require("../models/User");
 const sgMail = require("@sendgrid/mail");
 
 class DepartmentPostsController {
@@ -23,13 +24,14 @@ class DepartmentPostsController {
       await newPost.save();
 
       if (publish) {
-        // const query = await User.distinct('email');
-        sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-        const msg = {
-          to: ["oleg.bragin.01@gmail.com", "baxi585@gmail.com"], // Change to your recipient
-          from: "dimonaWorkersSite@gmail.com", // Change to your verified sender
-          templateId: "d-9d56dbe7304040858371bb8495de7705",
-        };
+        const query = await User.distinct('email',{department:department});
+        console.log(query);
+        // sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+        // const msg = {
+        //   to: ["oleg.bragin.01@gmail.com", "baxi585@gmail.com"], // Change to your recipient
+        //   from: "dimonaWorkersSite@gmail.com", // Change to your verified sender
+        //   templateId: "d-9d56dbe7304040858371bb8495de7705",
+        // };
         // sgMail
         //   .send(msg)
         //   .then(() => {
