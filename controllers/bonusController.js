6c03e2049a7f9e0c25714ef05fd487a470532119
olegbergin <oleg.bragin.01@gmail.com>
2022-11-23 11:23:34 +0200

    bonus org

--- controllers/bonusController.js
+++ controllers/bonusController.js
@@ -3,12 +3,31 @@ const Benifits = require("../models/Benifits.js");
 require('dotenv').config()
 
 class bonusController {
-    async getBonuses (req, res) {
+    async getBonuses(req, res) {
         const benifits = await Benifits.find();
         res.send(benifits);
-        
+
     }
 
+    async createBonus (req, res)  {
+        req.body.title &&
+        req.body.description &&
+        req.body.image &&
+        req.body.link &&
+        req.body.date &&
+        req.body.linktitle
+          ? Benifits.create(req.body)
+              .then(res.json({ message: "הטבה עודכנה בהצלחה!" }))
+              .catch(e)
+          : res.json({ error: "שגיאה!" });
+    }
+
+    async deleteBonus (req, res) {
+        const trying = await Benifits.findOneAndDelete({ title: req.params.title });
+        trying
+          ? res.json({ message: "הטבה נמחקה בהצלחה" })
+          : res.json({ message: "שגיאה" });
+    }
 
 }
 module.exports = new bonusController();
