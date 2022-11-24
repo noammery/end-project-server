0a4f20a0e9260c44a380561b08f337593dadce7f
olegbergin <oleg.bragin.01@gmail.com>
2022-11-24 11:17:09 +0200

    mail send

--- controllers/departPostController.js
+++ controllers/departPostController.js
@@ -25,21 +25,21 @@ class DepartmentPostsController {
 
       if (publish) {
         const query = await User.distinct('email',{department:department});
-        console.log(query);
-        // sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-        // const msg = {
-        //   to: ["oleg.bragin.01@gmail.com", "baxi585@gmail.com"], // Change to your recipient
-        //   from: "dimonaWorkersSite@gmail.com", // Change to your verified sender
-        //   templateId: "d-9d56dbe7304040858371bb8495de7705",
-        // };
-        // sgMail
-        //   .send(msg)
-        //   .then(() => {
-        //     console.log('Email sent')
-        //   })
-        //   .catch((error) => {
-        //     console.error(error)
-        //   })
+        // console.log(query);
+        sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+        const msg = {
+          to: query, // Change to your recipient
+          from: "dimonaWorkersSite@gmail.com", // Change to your verified sender
+          templateId: "d-9d56dbe7304040858371bb8495de7705",
+        };
+        sgMail
+          .send(msg)
+          .then(() => {
+            console.log('Email sent')
+          })
+          .catch((error) => {
+            console.error(error)
+          })
       }
       return res.json({ message: "פוסט נוצר בהצלחה" });
     } catch (e) {
